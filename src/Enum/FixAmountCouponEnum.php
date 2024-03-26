682741940b369b8d7e05f14cf13aaa9856ae809a
Mihail Serov <serov@spiks.ru>
2024-03-26 11:14:03 +0300

    feat: add enums

---
+++ src/Enum/FixAmountCouponEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+enum FixAmountCouponEnum: string
+{
+    case EURO_1 = 'euro_1';
+    case EURO_3 = 'euro_3';
+    case EURO_5 = 'euro_5';
+}
---
+++ src/Enum/PercentCouponEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+enum PercentCouponEnum: string
+{
+    case PERCENT_10 = 'percent_10';
+    case PERCENT_20 = 'percent_20';
+    case PERCENT_30 = 'percent_30';
+}
