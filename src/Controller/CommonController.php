7807a900023abefc78299462b3ff04e41f28abd6
Mihail Serov <serov@spiks.ru>
2024-03-26 11:23:20 +0300

    feat: purchase action

--- src/Controller/CommonController.php
+++ src/Controller/CommonController.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Exception\PurchaseFailException;
 use App\Form\CalculatePriceForm;
+use App\Form\PurchaseForm;
 use App\Service\CalculatePriceService;
+use App\Service\PurchaseService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -36,5 +39,33 @@ class CommonController extends AbstractController
         );
 
         return $this->json(data: ['calculatedPrice' => $calculatedPrice], status: 200);
+    }
+
+    #[Route('/purchase', methods: ['POST'])]
+    public function purchase(Request $request, PurchaseService $purchaseService): Response
+    {
+        $form = $this->createForm(PurchaseForm::class);
+        $form->submit($request->getPayload()->all());
+
+        if (!$form->isValid()) {
+            $errors = (string) $form->getErrors(deep: true, flatten: false);
+
+            return $this->json(data: ['errors' => $errors], status: 400);
+        }
+
+        $data = $form->getData();
+
+        try {
+            $purchaseService->purchase(
+                product: $data['product'],
+                taxNumber: $data['taxNumber'],
+                couponEnum: $data['couponCode'],
+                paymentProcessorAdapterEnum: $data['paymentProcessor'],
+            );
+        } catch (PurchaseFailException $exception) {
+            return $this->json(data: ['errors' => $exception->getMessage()], status: 400);
+        }
+
+        return $this->json(data: [], status: 200);
     }
 }
